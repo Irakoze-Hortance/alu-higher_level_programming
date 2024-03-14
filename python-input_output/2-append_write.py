feffe1b457ba718fc934ed88771c53366a5d6113
Irakoze-Hortance <hortanceirakoze@gmail.com>
2024-03-14 16:07:56 +0200

    files

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-'''appends a string at the end of a text file
-(UTF8) and returns the
-number of characters added:
-'''
+'''Appends a string at the end of a text file (UTF8) and returns the number of characters added'''
 
 
 def append_write(filename="", text=""):
-    '''function to write text to a file'''
+    '''Function to write text to a file'''
     with open(filename, 'a+') as f:
         return f.write(text)
-    
--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-'''A function that reads a json file'''
+'''A function that reads a JSON file'''
 
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    '''saves a doc to a json format'''
+    '''Saves a doc to a JSON format'''
     with open(filename, 'w+') as f:
         return json.dump(my_obj, f)

c896c4b74386bd2669244e0e6e001acac4be68d3
Irakoze-Hortance <hortanceirakoze@gmail.com>
2024-03-14 15:02:23 +0200

    python inheritance

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''A function that returns the available atts and methods of an object '''
+'''A function that returns the available attributes and methods of an object'''
 
 
 def lookup(obj):
-    '''Take an object and return its atts and methods'''
+    '''Take an object and return its attributes and methods'''
     return list(dir(obj))

9ef8d8308612194a4b79c0bd5812ac73dcad9528
Irakoze-Hortance <hortanceirakoze@gmail.com>
2024-03-21 22:39:25 +0200

    Pleasee

--- python-network_0/2-delete.sh
+++ python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that sends DELETE requests and displays the body of response
-curl -sX DELETE $1
+# send a delete request to a url 
+curl -sX DELETE "$1"
--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# curl to display all HTTP methods the server accepts
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+# get all the allowed methods 
+curl -sI "$1" | grep "Allow: " | cut -d " " -f 2-

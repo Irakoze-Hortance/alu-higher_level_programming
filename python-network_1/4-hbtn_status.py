f22198f9528c62640da0b44b949b8d8850014818
Irakoze-Hortance <hortanceirakoze@gmail.com>
2024-03-22 22:59:53 +0200

    fourth

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Python script that fetchs."""
-
-
+"""fetches https://intranet.hbtn.io/status using requests"""
 import requests
+
+
 if __name__ == "__main__":
-    requ = requests.get('https://intranet.hbtn.io/status')
+    """fetches https://intranet.hbtn.io/status using requests"""
+    r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(requ.text)))
-    print("\t- content: {}".format(requ.status_code))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))

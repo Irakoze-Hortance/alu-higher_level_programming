65795ea6e1924c84ab6897454fcbcf37f884948e
Irakoze-Hortance <hortanceirakoze@gmail.com>
2024-03-25 11:09:45 +0200

    url

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -11,12 +11,10 @@ headers = {
 
 
 req = urllib.request.Request(url, headers=headers)
-try:
-    with urllib.request.urlopen(req) as response:
-        content = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
-except urllib.error.HTTPError as e:
-    print(f"Error: {e.code} - {e.reason}")
+with urllib.request.urlopen(req) as response:
+    content = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode("utf-8")))
+

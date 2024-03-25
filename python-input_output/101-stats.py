c5643cd7e239e6aef4db2b1f031944af3af368ef
Irakoze-Hortance <hortanceirakoze@gmail.com>
2024-03-25 19:00:28 +0200

    test

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,73 +1,37 @@
 #!/usr/bin/python3
+""" reads stdin line by line and computes metrics:
 """
-script to parse and collect stats on web server logs
-"""
-
 import sys
-import signal
 
-
-def summarise(total_file_size, status_codes_frequency):
-    # Print the file size
-    print("File size:", total_file_size)
-
-    # Print status codes
-    keys = list(status_codes_frequency.keys())
-    keys.sort()
-    for status_code in keys:
-        print(f'{status_code}: {status_codes_frequency[status_code]}')
-
-
-def main():
-    status_codes_frequency = {}
-    total_file_size = 0
-    count = 0
-
-    # Summarise and reset on SIGINT signal
-    def on_sig_int():
-        nonlocal status_codes_frequency
-        nonlocal total_file_size
-
-        summarise(total_file_size, status_codes_frequency)
-
-    signal.signal(signal.SIGINT, on_sig_int)
-
-    # Loops each line in stdin
+file_size = 0
+status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
+               "403": 0, "404": 0, "405": 0, "500": 0}
+iter = 0
+try:
     for line in sys.stdin:
-        # parse line to extract info
-        try:
-            fields = line.strip().split()
-            status_code = int(fields[-2])
-            file_size = int(fields[-1])
+        iter += 1
+        line_list = line.split()
+        if len(line_list) >= 2:
+            if line_list[-2] in status_code:
+                status_code[line_list[-2]] += 1
+            try:
+                file_size += int(line_list[-1])
+            except FileNotFoundError:
+                pass
 
-            # Checking if status code is not valid
-            if type(status_code) is not int:
-                raise Exception()
+        if iter % 10 == 0:
+            print(f"File size: {file_size:d}")
+            for key, value in sorted(status_code.items()):
+                if value:
+                    print(f"{key}: {value:d}")
 
-            if status_code < 200 or status_code >= 600:
-                raise Exception()
+    print(f"File size: {file_size:d}")
+    for key, value in sorted(status_code.items()):
+        if value:
+            print(f"{key}: {value:d}")
 
-            # Add status_code to our hash
-            if status_code in status_codes_frequency:
-                status_codes_frequency[status_code] += 1
-            else:
-                status_codes_frequency[status_code] = 1
-
-            # increase total_file_size
-            total_file_size += file_size
-        except:
-            pass
-        finally:
-            # increment count
-            count += 1
-
-            # check if we should summarise
-            if count % 10 == 0:
-                summarise(total_file_size, status_codes_frequency)
-
-    # If we run out lines before a sigint or get a multiple of 10
-    # We just summarise what we currently have
-    summarise(total_file_size, status_codes_frequency)
-
-if __name__ == "__main__":
-    main()
+except KeyboardInterrupt:
+    print(f"File size: {file_size:d}")
+    for key, value in sorted(status_code.items()):
+        if value:
+            print(f"{key}: {value:d}")

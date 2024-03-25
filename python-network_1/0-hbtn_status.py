b0e644e22c42c8fc30fe2a653ac1b11e5557f334
Irakoze-Hortance <hortanceirakoze@gmail.com>
2024-03-25 10:31:42 +0200

    url

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 import urllib.request
 """ Fetches form url"""
 
-url = 'https://alu-intranet.hbtn.io/status'
+url = 'https://intranet.hbtn.io/status'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     '\n    AppleWebKit/537.36 (KHTML, like Gecko)'

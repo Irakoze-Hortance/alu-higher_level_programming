6258e4bab2a657827de2735b26cac78e5a6d48ce
Irakoze-Hortance <hortanceirakoze@gmail.com>
2024-03-25 11:21:01 +0200

    OK

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -14,5 +14,5 @@ with urllib.request.urlopen(req) as response:
     content = response.read()
     print("Body response:")
     print("\t- type: {}".format(type(content)))
-    print("\t- content: OK")
-    print("\t- utf8 content: OK")
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode("utf-8")))

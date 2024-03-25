b5c54ae7f532bda7247571a7acad7782c1daea3f
Irakoze-Hortance <hortanceirakoze@gmail.com>
2024-03-25 11:11:47 +0200

    url

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,13 +2,12 @@
 import urllib.request
 """ Fetches from https://alu-intranet.hbtn.io/status"""
 
-url = 'https://alu-intranet.hbtn.io/status'
+url = 'https://intranet.hbtn.io/status'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     '\n    AppleWebKit/537.36 (KHTML, like Gecko)'
     '\n    Chrome/99.0.4844.84 Safari/537.36',
 }
-
 
 req = urllib.request.Request(url, headers=headers)
 with urllib.request.urlopen(req) as response:
@@ -17,4 +16,3 @@ with urllib.request.urlopen(req) as response:
     print("\t- type: {}".format(type(content)))
     print("\t- content: {}".format(content))
     print("\t- utf8 content: {}".format(content.decode("utf-8")))
-
